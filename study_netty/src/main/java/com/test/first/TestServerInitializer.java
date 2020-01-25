86a03252a934d6756977e3a1db297c579e0e2d63
serenityma <846288113@qq.com>
2020-01-25 11:07:36 +0800

    study netty

---
+++ study_netty/src/main/java/com/test/first/TestServerInitializer.java
@@ -0,0 +1,15 @@
+package com.test.first;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+        ChannelPipeline pipeline = socketChannel.pipeline();
+        pipeline.addLast("HttpServerCodec", new HttpServerCodec());
+        pipeline.addLast("TestHttpServerHandler", new TestHttpServerHandler());
+    }
+}
